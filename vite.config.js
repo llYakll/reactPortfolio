7736e04e50b1ea4682cd4ad418b3f213edf359fb
llYakll <150628047+llYakll@users.noreply.github.com>
2024-05-12 15:38:50 -0500

    added all changes made since last push, think i forgot to push

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsDir: 'assets', // This specifies where to put assets like images
-    rollupOptions: {
-      input: './src/index.js', // Point this to your main JavaScript file
-    },
-  },
-});
+})
